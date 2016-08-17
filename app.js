da8ad42547b4fd09d202e9a1d100108d506bd422
Kevin Van de Mieroop <kevinvdm@Kevins-MBP.lan>
2016-08-17 21:48:37 +0200

    serial port functionality added

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 var mqtt    = require('mqtt');
 var client  = mqtt.connect('mqtt://localhost:1883');
- 
+var SerialPort = require("serialport");
+var port = new SerialPort("/dev/cu.usbmodem1421", {
+  baudRate: 9600
+});
 
  // subscribe
 client.on('connect', function () {
   client.subscribe('presence');
-  client.publish('presence', 'Publish victory!');
+  port.on('open', function(){
+    console.log('Serial Port Opend');
+    port.on('data', function(data){
+        client.publish('presence', data);
+    });
+  });
 });
- 
+
  // verify subscription
 client.on('message', function (topic, message) {
   // message is Buffer
   console.log(message.toString());
-  console.log(topic);
   client.end();
 });
