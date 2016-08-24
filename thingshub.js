1aa72f223c0f5c7b0f046d267d7ba8c9fd58e904
Muldoser <michielmldr@gmail.com>
2016-08-24 16:29:10 +0200

    List available interfaces added and functional

--- thingshub.js
+++ thingshub.js
@@ -1,11 +1,12 @@
 #! /usr/bin/env node
-var program = require('commander');
-var shell = require("shelljs");
+var 	program = require('commander'),
+		shell = require("shelljs"),
+		SerialPort = require('serialport');
 
 program
   .version('0.0.1')
   .option('-l, --list-attached-things', 'List all the attached things')
-  .option('-i, --list-available-interfaces', 'List all the available interfaces');
+  .option('-L, --list-available-interfaces', 'List all the available interfaces');
 
 program
   .command('attach')
@@ -19,5 +20,23 @@ if (program.listAttachedThings) {
   console.log('Attached things: None you doofus')
 };
 if (program.listAvailableInterfaces) {
-  console.log('There are no available interfaces')
+
+  SerialPort.list(function(err, ports){
+
+    if(err){
+      console.log('An error occurred while looking fore available interfaces: ' + err);
+      return null;
+    }
+  	if(ports.length > 0){
+      console.log('All ports: ' + JSON.stringify(ports));
+      ports.forEach(function(port){
+        console.log(port.comName);
+        console.log(port.manufacturer);
+      });
+    }
+    else{
+      console.log('There are no available interfaces');      
+    }
+  });
+
 };
