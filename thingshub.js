794f865ea089d4837538fed71c61ac44703d9b6c
Kevin Van de Mieroop <kevinvdm@Kevins-MacBook-Pro.local>
2016-08-24 20:48:13 +0200

    preparing to close feature

--- thingshub.js
+++ thingshub.js
@@ -3,6 +3,7 @@ var program = require('commander');
 var shell = require("shelljs");
 var fs = require('fs');
 var stringify = require('json-stringify-safe');
+var SerialPort = require("serialport");
 
 function appendObject(obj){
   var configFile = fs.readFileSync('./data.json');
@@ -15,15 +16,20 @@ function appendObject(obj){
 program
   .version('0.0.1')
   .option('-l, --list-attached-things', 'List all the attached things')
-  .option('-i, --list-available-interfaces', 'List all the available interfaces');
+  .option('-L, --list-available-interfaces', 'List all the available interfaces');
 
 program
   .command('attach <INTERFACE>')
   .option('-d, --dissect <DISSECTOR>', 'Specify a dissector')
   .option('-s, --silent', 'Turn silent mode on')
   .action(function(INTERFACE, options) {
+    var silence = false;
     console.log('Saving configuration...');
-    appendObject({name: INTERFACE, dissector: options.dissect});
+    if (options.silent)
+      {
+        silence = true;
+      }
+    appendObject({name: INTERFACE, dissector: options.dissect, silent: silence});
   });
 
 program.parse(process.argv);
@@ -32,5 +38,22 @@ if (program.listAttachedThings) {
   console.log('Attached things: None you doofus')
 };
 if (program.listAvailableInterfaces) {
-  console.log('There are no available interfaces')
+
+  SerialPort.list(function(err, ports){
+
+    if(err){
+      console.log('An error occurred while looking fore available interfaces: ' + err);
+      return null;
+    }
+  	if(ports.length > 0){
+      console.log('All ports: ' + JSON.stringify(ports));
+      ports.forEach(function(port){
+        console.log(port.comName);
+        console.log(port.manufacturer);
+      });
+    }
+    else{
+      console.log('There are no available interfaces');
+    }
+  });
 };
